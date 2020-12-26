40551d05132450098372bf609c4d1b6b10ed7399
derpmachine1 <ricksliu.mail@gmail.com>
2020-12-26 15:07:41 -0500

    Project page

--- public/js/about_me_photo.js
+++ public/js/about_me_photo.js
@@ -5,12 +5,12 @@ var photo = document.getElementById("main_photo");
 var photoAnimationID;
 photo.style.opacity = 0;
 function photoFadeIn() {
-    if (+photo.style.opacity < 1) {
-        photo.style.opacity = +photo.style.opacity + 0.01;
-        photoAnimationID = requestAnimationFrame(photoFadeIn);
-    } else {
-        cancelAnimationFrame(photoAnimationID);
-    }
+  if (+photo.style.opacity < 1) {
+    photo.style.opacity = +photo.style.opacity + 0.01;
+    photoAnimationID = requestAnimationFrame(photoFadeIn);
+  } else {
+    cancelAnimationFrame(photoAnimationID);
+  }
 }
 
 photoFadeIn();
--- public/js/index_heading.js
+++ public/js/index_heading.js
@@ -8,33 +8,33 @@ var headingString = heading.textContent;
 var headingAnimationID;
 heading.textContent = "";
 function headingAnimation1() {
-    setTimeout(function() {
-        if (heading.textContent.length < headingString.length) {
-            heading.textContent = heading.textContent + headingString.charAt(heading.textContent.length);
-            headingAnimationID = requestAnimationFrame(headingAnimation1);
-        } else {
-            cancelAnimationFrame(headingAnimationID);
-            headingAnimation2();
-        }
-    }, 1000 / 60);
+  setTimeout(function() {
+    if (heading.textContent.length < headingString.length) {
+      heading.textContent = heading.textContent + headingString.charAt(heading.textContent.length);
+      headingAnimationID = requestAnimationFrame(headingAnimation1);
+    } else {
+      cancelAnimationFrame(headingAnimationID);
+      headingAnimation2();
+    }
+  }, 1000 / 60);
 }
 
 // Adds cursor blinking animation if page is not in handheld mode
 function headingAnimation2() {
-    setTimeout(function() {
-        if ((window.innerWidth || document.body.clientWidth) > 800) {
-            if (heading.textContent.charAt(heading.textContent.length - 1) == "|") {
-                heading.textContent = headingString;
-            } else {
-                heading.textContent = headingString + "|";
-            }
-        } else {
-            if (heading.textContent.charAt(heading.textContent.length - 1) == "|") {
-                heading.textContent = headingString;
-            }
-        }
-        requestAnimationFrame(headingAnimation2);
-    }, 1000 / 2);
+  setTimeout(function() {
+    if ((window.innerWidth || document.body.clientWidth) > 800) {
+      if (heading.textContent.charAt(heading.textContent.length - 1) == "|") {
+        heading.textContent = headingString;
+      } else {
+        heading.textContent = headingString + "|";
+      }
+    } else {
+      if (heading.textContent.charAt(heading.textContent.length - 1) == "|") {
+        heading.textContent = headingString;
+      }
+    }
+    requestAnimationFrame(headingAnimation2);
+  }, 1000 / 2);
 }
 
 headingAnimation1();
